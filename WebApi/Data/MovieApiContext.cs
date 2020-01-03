e455f6807014cadddeea9790910ce3539ba60843
cchowdhary <cchowdhary@tavisca.in>
2020-01-03 11:36:51 +0530

    first commit

---
+++ WebApi/Data/MovieApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApi.Models
+{
+    public class MovieApiContext : DbContext
+    {
+        public MovieApiContext (DbContextOptions<MovieApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApi.Models.Movie> Movie { get; set; }
+    }
+}
